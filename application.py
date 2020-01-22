4a683e75edd480b87d274ea5a1dc5a039a7efb94
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-01-21 21:54:38 -0300

    EBS fixes

--- application.py
+++ application.py
@@ -7,5 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, basedir)
 
-from api import app as application
+from translator import app as application
 application.secret_key = os.urandom(64)
