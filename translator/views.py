8a54a58254b7a6d1eb47a36d1e7115c2c918533f
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-05-02 18:35:45 -0300

    APPVERSION fix

--- translator/views.py
+++ translator/views.py
@@ -2,7 +2,7 @@ from flask import jsonify, redirect, request, render_template
 from translator import app
 from translator.translate import get_call_from_curl, InvalidCurl, UnkownOption, get_powershell_from_call
 
-APP_VERSION = app.config["APP_VERSION"]
+APPVERSION = app.config["APPVERSION"]
 
 @app.route('/')
 def index():
