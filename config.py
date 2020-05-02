3427f8604cb956f7d0cac7cafb41111e7203d7ab
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-05-02 18:11:09 -0300

    APPVERSION through file

--- config.py
+++ config.py
@@ -3,4 +3,8 @@ import os
 import logging
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-APP_VERSION = os.environ.get("APPVERSION", default="Dev")
+if "APPVERSION" in os.listdir():
+    with open("APPVERSION", "r") as f:
+        APPVERSION = f.read()
+else:
+    APPVERSION = "Dev"
