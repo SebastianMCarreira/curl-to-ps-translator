8da5ba519235c2ea7938d0e108d042fb6d5ec42d
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-05-02 19:16:06 -0300

    Invalid curl return

--- translator/views.py
+++ translator/views.py
@@ -6,15 +6,16 @@ APPVERSION = app.config["APPVERSION"]
 
 @app.route('/')
 def index():
-    return render_template("index.html", app_version=APP_VERSION)
+    return render_template("index.html", app_version=APPVERSION)
 
 @app.route("/translate/", methods=["POST"])
 def translate():
     print(request.data)
     # try:
     call = get_call_from_curl(request.data.decode("utf-8"))
-    return get_powershell_from_call(call)
-    # except InvalidCurl:
-    #     return "Bad cURL"
+    try:
+        return get_powershell_from_call(call)
+    except InvalidCurl:
+        return "Bad cURL"
     # except UnkownOption:
     #     return "Unkown Option"
