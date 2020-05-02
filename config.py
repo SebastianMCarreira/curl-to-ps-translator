46f5d11f3c299c3b51f3e0c660f608016ccd874d
SebastianMCarreira <sebastian.m.careira@gmail.com>
2020-05-02 15:58:23 -0300

    App version

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 import logging
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_VERSION = os.environ.get("APPVERSION", default="Dev")
--- translator/views.py
+++ translator/views.py
@@ -2,9 +2,11 @@ from flask import jsonify, redirect, request, render_template
 from translator import app
 from translator.translate import get_call_from_curl, InvalidCurl, UnkownOption, get_powershell_from_call
 
+APP_VERSION = app.config["APP_VERSION"]
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", app_version=APP_VERSION)
 
 @app.route("/translate/", methods=["POST"])
 def translate():
